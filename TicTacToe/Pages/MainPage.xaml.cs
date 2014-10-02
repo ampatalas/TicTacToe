dd47ac3593c240e910e64c8b3eb9221b04dc8e71
ampatalas <a.m.patalas@gmail.com>
2014-10-02 17:23:29 +0200

    Add all basic files and two pages of application.

---
+++ TicTacToe/Pages/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using TicTacToe.Resources;
+
+namespace TicTacToe
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+
+        String color;
+        String symbol;
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            color = null;
+            symbol = null;
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            String uriAddress = "/Pages/PlayPage.xaml";
+            if (color == null || symbol == null)
+            {
+                MessageBox.Show("You didn't pick color or symbol!");
+            }
+            else
+            {
+                uriAddress += "?color=" + color + "&symbol=" + symbol;
+                NavigationService.Navigate(new Uri(uriAddress, UriKind.Relative));
+            }
+        }
+
+        private void RedColor_Checked(object sender, RoutedEventArgs e)
+        {
+            color = "red";
+        }
+
+        private void WhiteColor_Checked(object sender, RoutedEventArgs e)
+        {
+            color = "white";
+        }
+
+        private void NoughtSymbol_Checked(object sender, RoutedEventArgs e)
+        {
+            symbol = "nought";
+        }
+
+        private void CrossSymbol_Checked(object sender, RoutedEventArgs e)
+        {
+            symbol = "cross";
+        }
+    }
+}
---
+++ TicTacToe/Pages/PlayPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media;
+
+namespace TicTacToe.Pages
+{
+    public partial class PlayPage : PhoneApplicationPage
+    {
+        String symbol;
+        Color color;
+
+        public PlayPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (this.NavigationContext.QueryString.ContainsKey("color") && this.NavigationContext.QueryString.ContainsKey("symbol"))
+            {
+                symbol = this.NavigationContext.QueryString["symbol"];
+                String colorString = this.NavigationContext.QueryString["color"];
+                color = getColorFromString(colorString);
+                SymbolAndColorMessage.Text = "You are playing as " + symbol + " and your color is " + colorString + ".";
+            }
+        }
+
+        private Color getColorFromString(string color)
+        {
+            if (color.Equals("red")) return System.Windows.Media.Colors.Red;
+            else return System.Windows.Media.Colors.White;
+        }
+
+        private void TicTacToeTap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            Canvas canvasSender = sender as Canvas;
+            String name = canvasSender.Name;
+        }
+    }
+}
